db58281f61b6d426d1f2f57c68843f4c3f590ff6
Paul Copa <admin@MacBook-Air-Paul.local>
2026-09-14 23:04:27 +0200

    Demo 1.0

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ const scriptUrls = [
     "js/rmmz_sprites.js",
     "js/rmmz_windows.js",
     "js/plugins.js",
-    "js/mobile-controls.js?v=9"
+    "js/mobile-controls.js?v=10"
 ];
 const effekseerWasmUrl = "js/libs/effekseer.wasm";
 
